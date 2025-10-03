be18e1e31e3b6ef1485f51612853f561ed2a78d6
Ahmed Hassan <ahmedhassan.allatif@gmail.com>
2025-10-03 10:46:08 +0300

    minor-fix-4

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,30 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // 1. 💡 React and Router Core
+          "react-core": [
+            "react",
+            "react-dom",
+            "react-router",
+            "react-router-dom",
+          ],
+          // 2. 💡 Data & State Management
+          "data-state": ["@tanstack/react-query", "zustand", "axios"],
+
+          // 3. 💡 UI Vendors
+          "ui-vendors": [
+            "lucide-react",
+            "class-variance-authority",
+            "clsx",
+            "tailwind-merge",
+          ],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
 });
